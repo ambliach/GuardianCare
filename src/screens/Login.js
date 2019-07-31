2c20d967359e053b01d6e9d89948686310a96e64
Joseph Millan <https://github.com/JMillan13>
2019-07-30 20:27:37 -0400

    update styles

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -78,7 +78,7 @@ class Login extends Component {
       <Container className="my-5">
         <Row className="justify-content-center">
           <Form>
-            <h3>Login to MyApp</h3>
+            <h3>Login to GuardianCare</h3>
             <Form.Group>
               <Form.Label htmlFor="email">
                 Email
